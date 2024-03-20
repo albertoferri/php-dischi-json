d81476386e53e929aa5570b4383a1c2e9ac3f4b8
albertoferri <falberto99@gmail.com>
2024-03-20 17:05:43 +0100

    aggiunta semplice stilizzazione css

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+
     <!-- vue 3 -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
@@ -17,22 +20,29 @@
 </head>
 <body>
 
-    <div id="app">
-        <div class="container py-5">
-            <div class="row row-gap-4">
-                <div class="col-4" v-for="disc in discs">
+    <div id="app" style="background-color: #1d2d3c;">
+
+        <div class="container-fluid px-0">
+            <nav class="d-flex align-items-center px-5 py-2">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+            </nav>
+        </div>
+
+        <div id="disc-box" class="container py-5">
+            <div class="row row-cols-sm-2 row-cols-lg-3 row-gap-4">
+                <div class="col" v-for="disc in discs">
                     <div class="card">
-                        <img :src="disc.poster" class="card-img-top" alt="Cover">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ disc.title }}</h5>
-                            <p class="card-text">{{ disc.author }}</p>
-                            <p class="card-text">{{ disc.year }}</p>
-                            <p class="card-text">{{ disc.genre }}</p>
+                        <img :src="disc.poster" class="card-img-top p-5" alt="Cover">
+                        <div class="card-body px-4 pt-0 pb-5 text-center ">
+                            <h4 class="card-title"><strong>{{ disc.title }}</strong></h4>
+                            <h5 class="card-text">{{ disc.author }}</h5>
+                            <h4 class="card-text"><strong>{{ disc.year }}</strong></h4>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        
     </div>
 
 
